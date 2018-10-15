81133c685d0a09e96cb7ffd3e1ce992cd7cee02a
Amrit Prasad <amrit.prasad@mail.utoronto.ca>
2018-10-15 17:14:06 +0000

    columns derived

---
+++ constants.py
@@ -0,0 +1,30 @@
+#Raw dictionaries straight from the tables
+
+WORDS_TO_COMPONENT = {
+"Bookmark":	"Book mark, favorite, marked site".split(','),
+"Firefox":  "Browser	Firefox, Browser, Like, Update, Touch, mobile, gesture, Use, Thank, icon, Menu, button, fast, app, support, extension, addon, integration, look, interface, logo, Quantum, design,  menu, version, ui, safari, chrome, navigation, happy, hate, suck, pretty,beautical,ugly, elegant,good looking,good-looking,nice,pleasant,awful,great,design, user interface, user experience, ux, slow,lag,laggy,quick,speed,smooth,crash, frozen,abort,abandon,freeze,dead,died,update,release,updated,new version,compatible, windows, mac".split(','),
+"History/cookies/cache":	"History,cookies,cache,cook".split(','),
+"Top Sites":	"top sites, topsite, top websites, frequent visit, frequent site, high visit".split(','),
+"Highlights":	"highlight, recent visit, recent site, recent bookmark, latest visit, latest site, latest bookmark".split(','),
+"Pocket":	"pocket, save video, recommended by pocket".split(','),
+"Slowness":	"slow,fast,lag,laggy,quick,speed,smooth".split(','),
+"Crashes":	"crash, frozen,abort,abandon,freeze,dead,died".split(','),
+"Color":	"color, red, orange, yellow, green, blue, purple, brown, magenta, tan, cyan, olive, maroon, navy, aquamarine, turquoise, silver, lime, teal, indigo, violet, pink, white, black".split(','),
+"Preferences":	"preferences, fonts&color, save files to, default browser, general, language, application download, performance settings, scrollling, default search engine, remember log in, history setting, suggest browing history, suggest bookmarks, suggest open tabs, cached web content, set tracking protection, firefox data collection and use, block dangerous content, block dangerous downloads,".split(','),
+"Top Sites":	"top sites, topsite, top websites, frequent visit, frequent site, high visit".split(','),
+"Highlights":	"highlight, recent visit, recent site, recent bookmark, latest visit, latest site, latest bookmark".split(','),
+"Pocket":	"pocket, save video, recommended by pocket".split(',')
+
+}
+
+
+WORDS_TO_ISSUE = {
+"speed related":	"slow,fast,lag,laggy,quick,speed,smooth".split(','),
+"crash related":	"crash, frozen,abort,abandon,freeze,dead,died".split(','),
+"UI":	"pretty,beautical,ugly, elegant,good looking,good-looking,nice,pleasant,awful,great,UI,user interface,design".split(','),
+"Update":	"update,release,updated,new version".split(','),
+"Compatibility":	"compatible".split(','),
+"Bug":	"doesn’t,doesnot,does not,didn't,did not,don't, donot, do not, can't, cannot, can not,couldn't, couldnot, could not, will not, won't, wouldn't, would not, wouldnot, isn't, not, are not, were not, was not, click,work,working,run,delete,save,find,open,close,turnon,turnoff,remove,clear,hide,add,edit,change,view,see,erase,move,enable,disable,login,log in,logon, log on, search,exit,leave,bookmark,mark,scan,pin,load,reload,share,sign in, signin,signout, sign out,reset,set".split(',')
+}
+
+
