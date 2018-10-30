f49008fcc18db213ea2822ceaa807275119656f1
Carol Y <yeungcarol.k@gmail.com>
2018-10-29 22:31:56 -0400

    split function work

--- spam_filter.py
+++ spam_filter.py
@@ -1,6 +1,34 @@
 #IMPORTS
 import os
+import pandas as pd
+from whoosh.analysis import *
 
 #SETTINGS
 DIRECTORY = ""
 OUTPUT = os.path.join(DIRECTORY, "output.csv")
+
+###functions to make:
+
+#1 text preparation - reading in negative , cleaning, stemming, lemitizing,
+# Make a new column and put it in there - may be a new function
+def text_preparation ():
+    ###CODE HERE
+    return
+
+#2 feature extraction
+def feature_extraction():
+    ###CODE HERE
+    return
+
+#3 training the classifier + predicting + scoring
+def train_spam_filter():
+    ###CODE
+    return
+
+#4 pass on the classifier, make it predict pased on input
+def spam_classifier():
+    ####CODE HERE
+    return
+
+
+
