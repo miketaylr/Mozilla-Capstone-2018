cae92ce089c6f1af9136f38a7d410257e4f3669e
Carol Y <yeungcarol.k@gmail.com>
2018-11-05 01:22:54 -0500

    Fixed format, run the clusters with sf_output

--- clustering.py
+++ clustering.py
@@ -29,8 +29,8 @@ import pprint
 
 # SETTINGS - Paths
 DIRECTORY = ""
-OUTPUT = os.path.join(DIRECTORY, "output.csv")
-#TODO: get a directory for the new spam removal output
+OUTPUT = os.path.join(DIRECTORY, "output_spam_filtered.csv")
+# TODO: get a directory for the new spam removal output
 
 
 def createIndex(schema):
@@ -40,7 +40,7 @@ def createIndex(schema):
     return index.create_in(indexDir, schema)
 
 
-def addFilesToIndex(indexObj, csvPath):
+def addFilesToIndex(indexObj, csvPath, csvColumnName):
     # open writer
     writer = indexObj.writer()
     # open csv
@@ -53,8 +53,7 @@ def addFilesToIndex(indexObj, csvPath):
         i = 0
         # read each row in file
         for row in csvreader:
-            #TODO: change this to the new column from spam removal
-            value = row["Negative Feedback"]
+            value = row[csvColumnName]  # i.e. "sf_output" or "Negative Feedback"
             if value != "" and isinstance(value, str):
                 doc[i] = value
                 writer.update_document(index=str(i), cell_content=value)
@@ -63,13 +62,12 @@ def addFilesToIndex(indexObj, csvPath):
 
 
 def createNormalizedMatrix():
-    ### Create Reader to read in csv file after spam removal,
-    #  read in the column from
+    # Create Reader to read in csv file after spam removal, read in the column from:
     schema = Schema(index=ID(stored=True),
                     cell_content=TEXT(stored=True))
     indexToImport = createIndex(schema)
     # TODO: put indexToImport into dataframe instead of going through whoosh
-    addFilesToIndex(indexToImport, OUTPUT)
+    addFilesToIndex(indexToImport, OUTPUT, "sf_output")
     myReader = indexToImport.reader()
     print("Index is empty?", indexToImport.is_empty())
     print("Number of indexed files:", indexToImport.doc_count())
@@ -77,28 +75,27 @@ def createNormalizedMatrix():
     # If you want to see top 25 docs, print them below:
     # [(docnum, doc_dict) for (docnum, doc_dict) in myReader.iter_docs()][0:25]
 
-    ### Index all words in feedback from csv
+    # Index all words in feedback from csv
     all_words = [term for term in myReader.field_terms("cell_content")]
     # Get term freq for specific term i.e. android
     # print(myReader.frequency("cell_content", "android"))
-    #  1000 most distinctive terms according by TF-IDF score
+    # 1000 most distinctive terms according by TF-IDF score
     mostDistinctiveWords = [term.decode("ISO-8859-1") for (score, term) in
                             myReader.most_distinctive_terms("cell_content", 1000)]
     # 1000 most frequent words
     mostFrequentWords = [term.decode("ISO-8859-1") for (frequency, term) in
                          myReader.most_frequent_terms("cell_content", 1000)]
 
-    ### Create a binary encoding of dataset based on the selected features (X)
+    # Create a binary encoding of dataset based on the selected features (X)
     # Go through each document --> tokenize that single document --> compare with total word list
-    # TODO: change to frequency encoding -> how much better would it be
+    # TODO: change to frequency encoding -> how much better would it be?
     tokenizer = RegexpTokenizer(r'\w+')
     df_rows = []
     word_list = mostFrequentWords
     with codecs.open(OUTPUT, "r", "ISO-8859-1") as csvfile:
         csvreader = csv.DictReader(csvfile)
         for i, row in enumerate(csvreader):
-            value = row["Negative Feedback"]
-            #TODO: change this to the new column from spam removal
+            value = row["sf_output"]
             if value != "" and isinstance(value, str):
                 file_words = tokenizer.tokenize(value)
                 df_rows.append([1 if word in file_words else 0 for word in word_list])
@@ -108,14 +105,15 @@ def createNormalizedMatrix():
         # length normalization
         X_norm = preprocessing.normalize(data, norm='l2')
         rcOfX = X.shape
-    return X_norm, rcOfX, myReader
+        # (r,c) = X.shape
+    return X_norm, rcOfX[0], myReader
 
 
 def kMeansClustering(X, numOfRows, myReader):
-    ### Run k-means
+    # Run k-means
     # Setting number of clusters to hopefully split comments into sets of avg(10 FB comments)
+    num_clusters = numOfRows / 10
     num_clusters = 20
-    # num_clusters = numOfRows/10
     # TODO: make this (^) more robust / logical
     kmeans = KMeans(n_clusters=num_clusters)
     # Fitting the input data
@@ -128,8 +126,8 @@ def kMeansClustering(X, numOfRows, myReader):
     print(centroids)
     print(labelsAsNums)
 
-    ### NO NEED TO RUN IN FULL - START
-    ### Finding the best k - Elbow Method
+    # NO NEED TO RUN IN FULL - START
+    # Finding the best k - Elbow Method
     # k_average_inertia = []
     # local_inertia = []
     # random_states = []
@@ -151,10 +149,11 @@ def kMeansClustering(X, numOfRows, myReader):
     #     local_inertia = []
     #     print(i + 1, end=" ")
     # # Find the bend in this plot
+    # plt.close() # To start a new with a fresh graph
     # plt.plot(k_average_inertia)
-    ### NO NEED TO RUN IN FULL - END
+    # NO NEED TO RUN IN FULL - END
 
-    ###Hardcoded results for the Elbow Method
+    # Hardcoded results for the Elbow Method
     hardcoded_k_average_inertia = [996.120161061241,
                                    971.7098230964491,
                                    952.7861704492907,
@@ -257,10 +256,12 @@ def kMeansClustering(X, numOfRows, myReader):
                                    731.249786453746]
     plt.plot(hardcoded_k_average_inertia)
 
-    ### Show the num of docs/FB in each cluster, show the docs closest to centroid
+    # Show the num of docs/FB in each cluster, show the docs closest to centroid
     unique, counts = np.unique(kmeans.labels_, return_counts=True)
     clusterCount = dict(zip(unique, counts))
+    plt.close() # To start a new with a fresh graph
     plt.bar(clusterCount.keys(), clusterCount.values(), color='g')
+    plt.savefig("clusterCountBarGraph.png")
     # counts
     # See top 5 vectors closest to cluster centroid for all clusters
     for j in range(num_clusters):
@@ -270,7 +271,7 @@ def kMeansClustering(X, numOfRows, myReader):
         print("Indices of top 5 documents:", ind)
         print(np.array([doc_dict for doc_dict in myReader.iter_docs()])[ind])
 
-    ### OTHER WORK - NOT USING
+    # OTHER WORK - NOT USING
     # the distance to the j'th centroid for each point in an array X
     # d = kmeans.transform(X)[:, j]
     # ind = np.argsort(d)[::-1][:5]
@@ -286,7 +287,7 @@ def kMeansClustering(X, numOfRows, myReader):
     # cosineScores = pd.DataFrame(similarity_matrix)
     # clusters = SpectralClustering(n_clusters = 5, affinity = 'precomputed').fit(cosineScores)
 
-    #TODO: need to decide what to return here from our clustering
+    # TODO: need to decide what to return here from our clustering
     return labelsAsNums
 
 
@@ -319,7 +320,7 @@ def labelClustersWKeywords(labels, myReader):
     # ### TODO: FIXXXXXXXXX BRUUUUHHHHHHHHHHHHHHHH
     # ### Pulling out key words to label cluster / understand what is in each cluster
     # # pull out documents of each cluster --> tf idf for key words
-    # # set test_cluster to 12 TODO: change based on largest cluster
+    # # set test_cluster to 12 # TODO: change based on largest cluster
     # test_cluster = 12
     # # indices for test cluster
     # indices = [index for index, clusterNum in enumerate(labels) if clusterNum == test_cluster]
@@ -345,15 +346,8 @@ labels = kMeansClustering (X_norm, numOfFB, readerForFullFB)
 # visualizeSpectural
 labelClustersWKeywords(labels, readerForFullFB)
 print('We done.')
-# Notes from Spencer:
-# Do mutual information to get words that are prominent in only 1 cluster
-# how popular a word is in general, then
-# of time it appears in 1 clusters / total # of times it appears in all  documents
-# can weigh the times it appears time 2 to make it more important
-# also look at how popular the word is in the cluster itself
-# if the words are the same in all cluster, get array the can ignore them
-# interesting if something like Mozilla is not in an array
-# LOOK INTO MI - give info specific to cluster
-# might also want to take those words out and re-cluster, might get better results
+
+
+
 
 
--- spam_filter.py
+++ spam_filter.py
@@ -14,8 +14,9 @@ import pickle
 
 # SETTINGS
 DIRECTORY = ""
-OUTPUT_SPAM_LABELLED = os.path.join(DIRECTORY, "outputSpamLabelled.csv")
-INPUT = os.path.join(DIRECTORY, "output.csv")
+OUTPUT_SPAM_LABELLED = os.path.join(DIRECTORY, "SpamLabelled.csv")
+INPUT = os.path.join(DIRECTORY, "output_pipeline.csv") # from pipeline.py... for now
+# TODO: merge this class with pipeline.py
 
 
 # 1 Text Preparation
@@ -37,8 +38,10 @@ def text_preparation(filename):
         print('Not empty!', df.shape)
     # Make a new column and put it in there - may be a new function
     df['sf_output'] = df.apply(clean_feedback, axis=1)
-    df.to_csv('output_new.csv', encoding='ISO-8859-1')
+    # # Move this csv create to below...
+    # df.to_csv('output_new.csv', encoding='ISO-8859-1')
     return df
+
 
 def text_preparation_unlabelled(filename):
     num_records = 5000
@@ -57,7 +60,8 @@ def text_preparation_unlabelled(filename):
         print('Not empty!', df.shape)
     # Make a new column and put it in there - may be a new function
     df['sf_output'] = df.apply(clean_feedback, axis=1)
-    df.to_csv('output_new.csv', encoding='ISO-8859-1')
+    # # Move this csv create to below...
+    # df.to_csv('output_new.csv', encoding='ISO-8859-1')
     return df
 
 
@@ -136,31 +140,36 @@ def k_cross_validate(X, y, num_tests):
         test_results.append(test_accuracy)
     return train_results, test_results, clf
 
+
 def save_classifier_model(clf):
     filename = "spamClassifier.sav"
     pickle.dump(clf, open(filename, 'wb'))
 
+
 def load_classifier(filename):
     return pickle.load(open(filename, 'rb'))
 
-def scoreNewData(clf, X):
+
+def score_new_data(clf, X):
     return clf.predict(X)
 
-def getNonSpamIndices(y):
+
+def get_nonspam_indices(y):
     npArr = np.array(y)
     x = np.where(npArr == 0)[0]
     return x
 
-def removeSpam(df, nsi):
+
+def remove_spam(df, nsi):
     new_df = df.iloc[nsi, :]
     return new_df
+
 
 # Get the model and check its accuracy
 print('We starting.')
 
-# Train Classifier on labelled data
-# NOTE: run this the first time if you don't have the classifier built
-
+# # Train Classifier on labelled data
+# # NOTE: run this the first time if you don't have the classifier built
 # df = text_preparation(OUTPUT_SPAM_LABELLED)
 # X = feature_extraction(df)
 # y = get_qrel(df)
@@ -172,9 +181,9 @@ print('We starting.')
 loaded_clf = load_classifier("spamClassifier.sav")
 new_df = text_preparation_unlabelled(INPUT)
 X = feature_extraction(new_df)
-y = scoreNewData(loaded_clf, X)
-nsi = getNonSpamIndices(y).tolist()
+y = score_new_data(loaded_clf, X)
+nsi = get_nonspam_indices(y).tolist()
 
-removeSpam(new_df, nsi).to_csv("spamRemoved.csv")
+remove_spam(new_df, nsi).to_csv("output_spam_filtered.csv")
 
 print('We done.')
