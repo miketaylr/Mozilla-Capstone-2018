da1d187a470f9bda3aeb36dbad224a7462c89f9f
DESKTOP-C5NO6L8\secon <amritpd@gmail.com>
2018-11-15 18:28:16 -0500

    3 tabs, working on search

--- dash-interface.py
+++ dash-interface.py
@@ -1,34 +1,64 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table as dt 
+import pandas as pd
+from dash.dependencies import Input, Output, State, Event
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+results_df = pd.read_csv("output.csv", encoding ="ISO-8859-1")
+
+print (results_df.shape)
+#print(df.columns)
+
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Mozilla Analytics'
 '''
 Dash apps are composed of 2 parts. 1st part describes the app layout.
 The 2nd part describes the interactivty of the app 
 '''
+
+tabs_styles = {
+    'height': '44px'
+}
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px',
+    'fontWeight': 'bold'
+}
+
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'color': 'white',
+    'padding': '6px'
+}
+
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1('Dash Tabs component demo'),
-    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
-        dcc.Tab(label='Trending Issues', value='tab-1-example'),
-        dcc.Tab(label='Commmon Issues', value='tab-2-example'),
-        dcc.Tab(label='Search', value='tab-3-example'),
-    ]),
-    html.Div(id='tabs-content-example'),
+app.layout = html.Div(children=[
     html.H1(
         children='Mozilla Customer Feedback Analytics Tool',
         style={
-            'textAlign': 'center'
+            'textAlign': 'center',
+            'color': 'orange'
 
         }
     ),
+    dcc.Tabs(id="tabs-styled-with-inline", value='tab-1', children=[
+        dcc.Tab(label='Trends', value='tab-1', style=tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Common Issues', value='tab-2', style=tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Search', value='tab-3', style=tab_style, selected_style=tab_selected_style),
+    ], style=tabs_styles),
+    html.Div(id='tabs-content-inline'),
+
 
     html.Div(children='Sentiment Breakdown using Dash/Plotly', style={
         'textAlign': 'center',
@@ -53,13 +83,13 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     )
 ])
 
-@app.callback(dash.dependencies.Output('tabs-content-example', 'children'),
-              [dash.dependencies.Input('tabs-example', 'value')])
+@app.callback(Output('tabs-content-inline', 'children'),
+              [Input('tabs-styled-with-inline', 'value')])
 
 def render_content(tab):
-    if tab == 'tab-1-example':
+    if tab == 'tab-1':
         return html.Div([
-            html.H3('Tab content 1'),
+            html.H3('Recent Trends'),
             dcc.Graph(
                 id='graph-1-tabs',
                 figure={
@@ -71,9 +101,9 @@ def render_content(tab):
                 }
             )
         ])
-    elif tab == 'tab-2-example':
+    elif tab == 'tab-2':
         return html.Div([
-            html.H3('Tab content 2'),
+            html.H3('Common Issues'),
             dcc.Graph(
                 id='graph-2-tabs',
                 figure={
@@ -84,6 +114,27 @@ def render_content(tab):
                     }]
                 }
             )
+        ])
+    elif tab == 'tab-3':
+        return html.Div([
+            html.H3('Search Raw Comments'),
+            html.Label('Enter Search Term:'),
+            dcc.Input(value='Type here', type='text'),
+            dcc.Graph(
+                id='graph-2-tabs',
+                figure={
+                    'data': [{
+                        'x': [1, 2, 3],
+                        'y': [5, 10, 6],
+                        'type': 'bar'
+                    }]
+                }
+            ),
+            dt.DataTable(
+                    rows=results_df,
+                    columns=results_df.columns,
+                    filterable=True,
+                    id='datatable')
         ])
 
 if __name__ == '__main__':
